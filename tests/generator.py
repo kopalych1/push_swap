0bb7673232f357804bfc3a2d8048acd58d487424
kopalych <kopalychmail@gmail.com>
2024-08-13 14:23:55 +0200

    Removing all the tests from the project

--- tests/generator.py
+++
@@ -1,7 +0,0 @@
-from random import sample
-from sys import argv
-ARRAY_MIN = 0
-ARRAY_MAX = 2001
-N_ELEMENTS = int(argv[1]) if len(argv) > 1 else 100
-
-print(" ".join(str(_) for _ in sample(range(max(-N_ELEMENTS, ARRAY_MIN), min(N_ELEMENTS, ARRAY_MAX)), N_ELEMENTS)))
